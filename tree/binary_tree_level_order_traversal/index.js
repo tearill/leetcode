d1622c75cf12aaf7a1f6ea4165bdeb234663a07c
tearill <1105317666@qq.com>
2020-03-11 18:17:37 +0800

    二叉树的层次遍历

---
+++ tree/binary_tree_level_order_traversal/index.js
@@ -0,0 +1,42 @@
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
+
+// [3,9,20,null,null,15,7]
+var a1 = new TreeNode(3)
+var a2 = new TreeNode(9)
+var a3 = new TreeNode(20)
+a1.left = a2
+a1.right = a3
+
+var a4 = new TreeNode(15)
+var a5 = new TreeNode(7)
+a3.left = a4
+a3.right = a5
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+var levelOrder = function(root) {
+  if (!root) return // 树为空时直接返回
+    let result = [] // 结果数组
+    function walk(node, level) { // 辅助函数遍历每一层的结点
+    if(result.length === level) {
+      result.push([]) // 初始化定型每一层的数组
+    }
+  result[level].push(node.val) // 每一层都是一个数组
+    if(node.left) {
+      walk(node.left, level + 1) // 访问左结点下一层
+    }
+    if(node.right) {
+      walk(node.right, level + 1) // 访问右节点下一层
+    }
+  }
+  walk(root, 0)
+  return result
+}
+
+console.log(levelOrder(a1))
---
+++ tree/binary_tree_level_order_traversal/readme.md
@@ -0,0 +1,15 @@
+# 二叉树的层次遍历  
+leetcode👉https://leetcode-cn.com/problems/binary-tree-level-order-traversal/  
+
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。  
+
+[3,9,20,null,null,15,7]  
+   | |  
+   \\/  
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+- 目标：返回一个按层次遍历的数组  
