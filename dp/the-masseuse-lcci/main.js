b143f6fd74b65b4cd0020dfdee3ddd5d7f54f44b
tearill <1105317666@qq.com>
2020-03-25 09:23:18 +0800

    完善新解法

--- dp/the-masseuse-lcci/main.js
+++ dp/the-masseuse-lcci/main.js
@@ -5,10 +5,14 @@
 var massage = function(nums) {
   let a = 0, b = 0
   for (let i = 0; i < nums.length; i++) {
-    let temp = b
-    b = Math.max(a + nums[i], b)
-    a = temp
+    // let temp = b
+    let c = Math.max(a + nums[i], b) // 第一个|第三个 和 第二个 比较
+    // b = Math.max(a + nums[i], b)
+    // a = temp
+    a = b // a 向后移动
+    b = c // b 作为当前最大值
   }
+  return b
 };
 
 console.log(massage([1, 2, 3, 1]))
